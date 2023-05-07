d6a5ab915ea6020a2a749c458199ea1cf27a9907
Arya <aryailham05@gmail.com>
2023-05-08 06:26:14 +0700

    Create String+Extensions.swift

---
+++ Submission Aplikasi Bentuk Katalog/Core/Extensions/String+Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  String+Extensions.swift
+//  Submission Aplikasi Bentuk Katalog
+//
+//  Created by Arya Ilham on 08/05/23.
+//
+
+import Foundation
+
+extension String {
+    var htmlToAttributedString: NSAttributedString? {
+        guard let data = data(using: .utf8) else { return nil }
+        do {
+            return try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil)
+        } catch {
+            return nil
+        }
+    }
+    var htmlToString: String {
+        return htmlToAttributedString?.string ?? ""
+    }
+}
+
