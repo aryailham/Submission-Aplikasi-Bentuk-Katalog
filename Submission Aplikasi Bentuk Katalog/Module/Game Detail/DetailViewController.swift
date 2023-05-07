ad30962fd6487025a71f7240390a7e1e44988d8a
Arya <aryailham05@gmail.com>
2023-05-07 21:28:19 +0700

    Create DetailViewController.swift

---
+++ Submission Aplikasi Bentuk Katalog/Module/Game Detail/DetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.swift
+//  Submission Aplikasi Bentuk Katalog
+//
+//  Created by Arya Ilham on 07/05/23.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
