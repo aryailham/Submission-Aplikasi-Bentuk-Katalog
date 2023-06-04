20dc9515841d1364110be9607f9981a6acf1c3ec
Arya <aryailham05@gmail.com>
2023-06-04 21:27:48 +0700

    Update GameCatalogRepository.swift

--- Submission Aplikasi Bentuk Katalog/Core/Data/GameCatalogRepository.swift
+++ Submission Aplikasi Bentuk Katalog/Core/Data/GameCatalogRepository.swift
@@ -6,17 +6,57 @@
 //
 
 import Foundation
+import RxSwift
 
 protocol GameCatalogRepository {
-    func getGameCatalog(completion: @escaping (Result<GameCatalogResponse?, Error>) -> ())
+    func getGameCatalog() -> Observable<[GameModel]>
 }
 
 class GameCatalogDefaultRepository {
-    private var remote: GameCatalogRemoteDataSource?
+    typealias instance = (GameCatalogLocalDataSource, GameCatalogRemoteDataSource) -> GameCatalogDefaultRepository
+    
+    private var remote: GameCatalogRemoteDataSource
+    private var local: GameCatalogLocalDataSource
+    
+    private init(local: GameCatalogLocalDataSource, remote: GameCatalogRemoteDataSource) {
+        self.local = local
+        self.remote = remote
+    }
+    
+    static let sharedInstance: instance = { localRepo, remoteRepo in
+        return GameCatalogDefaultRepository(local: localRepo, remote: remoteRepo)
+    }
 }
 
 extension GameCatalogDefaultRepository: GameCatalogRepository {
-    func getGameCatalog(completion: @escaping (Result<GameCatalogResponse?, Error>) -> ()) {
+    func getGameCatalog() -> Observable<[GameModel]> {
+//        return self.remote.getGameCatalog()
+//            .map({ responses in
+//                GameCatalogMapper.mapGameCatalogResponsetoEntity(gameResponse: responses)
+//            })
+//            .map { entities in
+//                self.local.addData(from: entities)
+//            }
+//            .flatMap { _ in
+//                self.local.getData()
+//                    .map { entities in
+//                        GameCatalogMapper.mapGameCatalogEntityToDomainModel(gameEntity: entities)
+//                    }
+//            }
         
+        return self.remote.getGameCatalog()
+            .map({ responses in
+                GameCatalogMapper.mapGameCatalogResponsetoEntity(gameResponse: responses)
+            })
+            .flatMap { entities in
+                self.local.addData(from: entities)
+            }
+            .flatMap { _ in
+                self.local.getData()
+                    .map { entities in
+                        GameCatalogMapper.mapGameCatalogEntityToDomainModel(gameEntity: entities)
+                    }
+            }
+
     }
 }
