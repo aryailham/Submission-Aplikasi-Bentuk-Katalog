3475008b7ec92b552ae0051c3865809148316504
Aryailhm <arya.ilham@misteraladin.com>
2023-06-05 12:46:30 +0700

    Update WishlistDefaultLocalDataSource.swift

--- Submission Aplikasi Bentuk Katalog/Core/Data/Local/WishlistDefaultLocalDataSource.swift
+++ Submission Aplikasi Bentuk Katalog/Core/Data/Local/WishlistDefaultLocalDataSource.swift
@@ -1,108 +1,91 @@
 //
-//  WishlistLocalDataSource.swift
+//  WishlistDefaultLocalDataSource.swift
 //  Submission Aplikasi Bentuk Katalog
 //
-//  Created by Arya Moehammad Ilham on 11/05/23.
+//  Created by Arya Moehammad Ilham on 05/06/23.
 //
 
 import Foundation
-import CoreData
+import RxSwift
+import RealmSwift
 
-class WishlistDefaultLocalDataSource {
-    lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "WishlistModel")
-        container.loadPersistentStores { storeDescription, error in
-            if let error = error as NSError? {
-                fatalError("error persistent container: \(error)")
-            }
-        }
-        return container
-    }()
+protocol WishlistLocalDataSource {
+    func getWishlistedGames() -> Observable<[WishlistEntity]>
+    func storeNewWishlistedGames(wishlistedGame: WishlistEntity) -> Observable<Bool>
+    func removeWishlistedGames(gameId: Int)
+    func checkIfWishlisted(gameId: Int) -> Observable<Bool>
+}
+
+class WishlistRealmLocalDataSource {
+    let realm: Realm?
     
-    static let shared = WishlistDefaultLocalDataSource()
-    
-    func getWishlistedGames(completion: @escaping ([Wishlist]) -> Void) {
-        let managedContext = persistentContainer.viewContext
-        
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Wishlist")
-        do {
-            let results = try managedContext.fetch(fetchRequest) as! [Wishlist]
-            completion(results)
-        } catch let error as NSError {
-            print("error fetch coredata, error: \(error)")
-        }
+    private init(realm: Realm?) {
+        self.realm = realm
     }
     
-    func storeNewWishlist(game: GameModel) {
-        let managedContext = persistentContainer.viewContext
-        
-        let newWishlist = Wishlist(context: managedContext)
-        newWishlist.id = Int64(game.id ?? 0)
-        newWishlist.name = game.name
-        newWishlist.rating = game.rating ?? 0
-        newWishlist.released = game.released
-        newWishlist.gameDescription = game.description
-        newWishlist.ratingTop = Int16(game.ratingTop ?? 0)
-        newWishlist.metacritic = Int16(game.metacritic ?? 0)
-        newWishlist.backgroundImage = game.backgroundImage
-        game.tags.forEach { tag in
-            let newTag = GameTags(context: managedContext)
-            newTag.id = Int64(tag.id ?? 0)
-            newTag.name = tag.name
-            newWishlist.addToTags(newTag)
-        }
-        
-        saveContext()
-    }
-    
-    func removeWishlistedGame(gameID: Int) {
-        let managedContext = persistentContainer.viewContext
-        
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Wishlist")
-        fetchRequest.predicate = NSPredicate(format: "id == \(gameID)")
-        fetchRequest.fetchLimit = 1
-        if let result = try? managedContext.fetch(fetchRequest), let game = result.first as? Wishlist {
-            managedContext.delete(game)
-        }
-        
-        saveContext()
-    }
-    
-    func checkIfWishlisted(gameID: Int) -> Bool{
-        let managedContext = persistentContainer.viewContext
-        
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Wishlist")
-        fetchRequest.predicate = NSPredicate(format: "id == \(gameID)")
-        fetchRequest.fetchLimit = 1
-        if let result = try? managedContext.fetch(fetchRequest) {
-            return !result.isEmpty
-        }
-        return false
-    }
-    
-    private func saveContext() {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                let nserror = error as NSError
-                fatalError("error save context: \(nserror)")
-            }
-        }
+    static let sharedInstance: (Realm?) -> WishlistLocalDataSource = { realmDatabase in
+        return WishlistRealmLocalDataSource(realm: realmDatabase)
     }
 }
 
-extension WishlistDefaultLocalDataSource: GameCatalogDetailRemoteDataSource {
-    func getGameDetails(id: Int, completion: @escaping (Result<GameModel?, Error>) -> Void) {
-        let managedContext = persistentContainer.viewContext
-        
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Wishlist")
-        fetchRequest.predicate = NSPredicate(format: "id == \(id)")
-        fetchRequest.fetchLimit = 1
-        if let result = try? managedContext.fetch(fetchRequest) {
-            let wishlist = GameMapper.MapGameEntityToDomainModel(gameEntity: result.first as! Wishlist)
-            completion(.success(wishlist))
+extension WishlistRealmLocalDataSource: WishlistLocalDataSource {
+    func getWishlistedGames() -> Observable<[WishlistEntity]> {
+        return Observable<[WishlistEntity]>.create { observer in
+            if let realm = self.realm {
+                let wishlistedGames = realm.objects(WishlistEntity.self)
+                observer.onNext(Array(wishlistedGames))
+                observer.onCompleted()
+            } else {
+                observer.onError(RealmError.readError)
+            }
+            
+            return Disposables.create()
+        }
+    }
+    
+    func storeNewWishlistedGames(wishlistedGame: WishlistEntity) -> Observable<Bool> {
+        return Observable<Bool>.create { observer in
+            if let realm = self.realm {
+                do {
+                    try realm.write({
+                        realm.add(wishlistedGame, update: .all)
+                    })
+                } catch let error {
+                    observer.onError(error)
+                }
+            }
+            return Disposables.create()
+        }
+    }
+    
+    func removeWishlistedGames(gameId: Int) {
+        if let realm = self.realm {
+            do {
+                try realm.write({
+                    if let objectToDelete = realm.object(ofType: WishlistEntity.self, forPrimaryKey: gameId) {
+                        realm.delete(objectToDelete)
+                    }
+                })
+            } catch let error {
+                print("remove wishlist error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func checkIfWishlisted(gameId: Int) -> Observable<Bool> {
+        return Observable<Bool>.create { observer in
+            if let realm = self.realm {
+                let data = realm.object(ofType: WishlistEntity.self, forPrimaryKey: gameId)
+                
+                if data != nil {
+                    observer.onNext(true)
+                } else {
+                    observer.onNext(false)
+                }
+                observer.onCompleted()
+            }
+            
+            return Disposables.create()
         }
     }
 }
