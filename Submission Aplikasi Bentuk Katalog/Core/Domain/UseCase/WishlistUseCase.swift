00952b5827ff55b9ba594973733e9cd204375d60
Aryailhm <arya.ilham@misteraladin.com>
2023-06-07 13:59:02 +0700

    Update WishlistUseCase.swift

--- Submission Aplikasi Bentuk Katalog/Core/Domain/UseCase/WishlistUseCase.swift
+++ Submission Aplikasi Bentuk Katalog/Core/Domain/UseCase/WishlistUseCase.swift
@@ -11,13 +11,6 @@ import RxSwift
 protocol WishlistUseCase {
     func getWishlistedGames() -> Observable<[GameModel]>
 }
-
-protocol WishlistDetailUseCase {
-    func storeNewWishlistedGames(wishlistedGame: GameModel) -> Observable<Bool>
-    func removeWishlistedGames(gameId: Int)
-    func checkIfWishlisted(gameId: Int) -> Observable<Bool>
-}
-
 class WishlistDefaultInteractor {
     let repository: WishlistRepository
     
@@ -29,19 +22,5 @@ class WishlistDefaultInteractor {
 extension WishlistDefaultInteractor: WishlistUseCase {
     func getWishlistedGames() -> Observable<[GameModel]> {
         return self.repository.getWishlistedGames()
-    }
-}
-
-extension WishlistDefaultInteractor: WishlistDetailUseCase {
-    func storeNewWishlistedGames(wishlistedGame: GameModel) -> Observable<Bool> {
-        return self.repository.storeNewWishlistedGames(wishlistedGame: wishlistedGame)
-    }
-    
-    func removeWishlistedGames(gameId: Int) {
-        self.repository.removeWishlistedGames(gameId: gameId)
-    }
-    
-    func checkIfWishlisted(gameId: Int) -> Observable<Bool> {
-        return self.repository.checkIfWishlisted(gameId: gameId)
     }
 }
